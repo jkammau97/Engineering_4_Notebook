85e4fec23867bafb5efd15c3165772fbdd9f3e5f
jkammau97 <56128669+jkammau97@users.noreply.github.com>
2021-09-20 10:45:54 -0400

    Add files via upload

---
+++ Python/quadratic_solver.py
@@ -0,0 +1,28 @@
+# Quadratic Solver
+
+# Written By: Bob Kammauff
+import math
+from math import sqrt
+
+def quadratic(x, y, z)
+    d = b*b-(4*a*c)
+    if d < 0:
+        print("no real roots")
+    else:
+        return
+    
+print("Quadratic Solver")
+print("Enter the coefficients for ax^2 + bx + c = 0")
+
+
+while True:
+    # find values for a b c
+    print("what's a?")
+    a = input()
+    print("What's b?")
+    b = input()
+    print("What's c?")
+    c = input()
+
+    
+    
