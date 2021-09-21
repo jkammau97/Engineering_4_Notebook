564d382d18c47beda1765c4120332a1f0c82b2cc
jkammau97 <56128669+jkammau97@users.noreply.github.com>
2021-09-21 10:14:09 -0400

    Update strings_and_loops_spicy.py

--- Python/strings_and_loops_spicy.py
+++ Python/strings_and_loops_spicy.py
@@ -1,4 +1,4 @@
 print("type in your text, then press enter:")
-for x in input():
-    if x == ' ': print('-')
-    else: print(x)
+for x in input(): # yeah this works for some reason
+    if x == ' ': print('-') # If it's a space, it turns into a dash
+    else: print(x) # prints thhe characters one by one
