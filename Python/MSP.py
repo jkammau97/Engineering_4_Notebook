c6dc7f06aa161fd6e13638129fe448f9ae7c2f0e
jkammau97 <56128669+jkammau97@users.noreply.github.com>
2021-09-28 10:43:29 -0400

    guessing part of game
    Still doesn't work; doesn't loop to the beginning

--- Python/MSP.py
+++ Python/MSP.py
@@ -1,11 +1,13 @@
 
-def printHangman(triesNum):
+def printHangman(triesNum): #program that prints out a man if there's an incorrect guess
     #---┐ 
     #   O 
     #  \|/
     #   | 
     #  / \
     print("---┐")
+    if triesNum == 0:
+        print('\n'*4) #prints space for hangman to exist
     if triesNum == 1:
         print("   O ")
         print("     ")
@@ -42,11 +44,38 @@ def printHangman(triesNum):
         print("   | ")
         print("  / \\")
 
-printHangman(0)
-printHangman(1)
-printHangman(2)
-printHangman(3)
-printHangman(4)
-printHangman(5)
-printHangman(6)
-printHangman(7)
+#Beginning
+
+print("Man-Shaped Pinata")
+print("Player 1, Enter a word:")
+# word = input() #get string of word
+word = "pineapple" #for debugging; I don't wanna enter the word every time
+print('\n' * 50)
+
+print("cleared screen! don't scroll up!")
+
+badGuesses = {} # Using a python set because they are unordered, and do not accept duplicates
+
+answerList = [] # make a list for what player 2 has for the answer so far
+
+for x in word:
+    answerList.append('_ ')
+
+while True:
+    printHangman(len(badGuesses))
+    answer = ""
+    for x in answerList:
+        answer = answer + x
+    print(answer)
+
+    print("Player 2, guess a letter:")
+
+    guess = input()
+    # the idea here is that we check if the character is in the word, then finds the position of the character in the word, which corresponds to that position in the list
+    # change the "_ " to the character to reveal it, then loop back to the Beginning
+    if guess in word:
+        i = word.find(guess)
+        while i > -1:
+            answerList[i] = guess + " "
+            i = word.find(guess)
+
