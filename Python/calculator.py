f6eb410f0e34ae506158686b46e8bc24108b4421
jkammau97 <56128669+jkammau97@users.noreply.github.com>
2021-10-25 09:40:16 -0400

    Commented

--- Python/calculator.py
+++ Python/calculator.py
@@ -12,15 +12,15 @@ def doMath(x, y ,z):
     if z == 4:
         return x / y
     if z == 5:
-        return x % y
+        return x % y # % is modulo; gives the remainder of the two numbers when divided.
 
 print("calculator Program")
 print("enter your first number: ")
-a = int(input())
+a = int(input()) #need to convert the input into a number so we can do stuff with it
 print("enter your second number: ")
-b = int(input())
+b = int(input()) #need to convert the input into a number so we can do stuff with it
 
-print("Sum:\t\t" + str(doMath(a,b,1)))
+print("Sum:\t\t" + str(doMath(a,b,1)))  #str() is needed because that's how it's gonna print it out
 print("Difference:\t" + str(doMath(a,b,2)))
 print("Product:\t" + str(doMath(a,b,3)))
 print("Quotient:\t" + str(doMath(a,b,4)))
