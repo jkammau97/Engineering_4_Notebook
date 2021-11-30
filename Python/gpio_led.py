fa97a167bd15f7f198dc7011160b1b5061aaf6d0
jkammau97 <56128669+jkammau97@users.noreply.github.com>
2021-11-30 10:04:46 -0500

    commented code

--- Python/gpio_led.py
+++ Python/gpio_led.py
@@ -2,19 +2,24 @@
 # RPi GPIO Pin Introduction
 # 11/16/21
 
+# https://raspberrypihq.com/making-a-led-blink-using-the-raspberry-pi-and-python/
+
 import RPi.GPIO as GPIO
 from time import sleep
+
+greenPin = 21
+redPin = 19
 
 GPIO.setmode(GPIO.BCM) #this sets my pin numbering scheme as
 # the BCM numbering scheme
 
-GPIO.setwarnings(False)
-GPIO.setup(19, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(21, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setwarnings(False)  # Ignore warning for now
+GPIO.setup(redPin, GPIO.OUT, initial=GPIO.LOW) #initializes the pin
+GPIO.setup(greenPin, GPIO.OUT, initial=GPIO.HIGH)
 while True:
-	GPIO.output(19, GPIO.HIGH) 
-	GPIO.output(21, GPIO.LOW) 
+	GPIO.output(redPin, GPIO.HIGH) # red on
+	GPIO.output(greenPin, GPIO.LOW) # green off
 	sleep(1) 
-	GPIO.output(19, GPIO.LOW) 
-	GPIO.output(21, GPIO.HIGH)
+	GPIO.output(redPin, GPIO.LOW) # red off
+	GPIO.output(greenPin, GPIO.HIGH) # green on
 	sleep(1)
