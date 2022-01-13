ca4723a0d9c39c5b045098f32c5228161361ecd2
jkammau97 <56128669+jkammau97@users.noreply.github.com>
2022-01-13 10:09:48 -0500

    First code outline

--- Python/GPIOPinsI2C.py
+++ Python/GPIOPinsI2C.py
@@ -23,42 +23,6 @@ lsm303 = Adafruit_LSM303.LSM303()
 # Alternatively you can specify the I2C bus with a bus parameter:
 #lsm303 = Adafruit_LSM303.LSM303(busum=2)
 
-print('Printing accelerometer & magnetometer X, Y, Z axis values, press Ctrl-C to quit...')
-while True:
-    # Read the X, Y, Z axis acceleration values and print them.
-    accel, mag = lsm303.read()
-    # Grab the X, Y, Z components from the reading and print them out.
-    accel_x, accel_y, accel_z = accel
-    mag_x, mag_y, mag_z = mag
-    print('Accel X={0}, Accel Y={1}, Accel Z={2}, Mag X={3}, Mag Y={4}, Mag Z={5}'.format(
-          accel_x, accel_y, accel_z, mag_x, mag_y, mag_z))
-    # Wait half a second and repeat.
-    time.sleep(0.5)
-    
-# ---
-
-# Copyright (c) 2014 Adafruit Industries
-# Author: Tony DiCola
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-import time
-
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_SSD1306
 
@@ -80,55 +44,44 @@ disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3d)
 # Initialize library.
 disp.begin()
 
-# Clear display.
-disp.clear()
-disp.display()
+while True:
+    # Clear display.
+    disp.clear()
+    disp.display()
 
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
-width = disp.width
-height = disp.height
-image = Image.new('1', (width, height))
+    # Create blank image for drawing.
+    # Make sure to create image with mode '1' for 1-bit color.
+    width = disp.width
+    height = disp.height
+    image = Image.new('1', (width, height))
 
-# Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
+    # Get drawing object to draw on image.
+    draw = ImageDraw.Draw(image)
 
-# Draw a black filled box to clear the image.
-draw.rectangle((0,0,width,height), outline=0, fill=0)
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-# Draw some shapes.
-# First define some constants to allow easy resizing of shapes.
-padding = 2
-shape_width = 20
-top = padding
-bottom = height-padding
-# Move left to right keeping track of the current x position for drawing shapes.
-x = padding
-# Draw an ellipse.
-draw.ellipse((x, top , x+shape_width, bottom), outline=255, fill=0)
-x += shape_width+padding
-# Draw a rectangle.
-draw.rectangle((x, top, x+shape_width, bottom), outline=255, fill=0)
-x += shape_width+padding
-# Draw a triangle.
-draw.polygon([(x, bottom), (x+shape_width/2, top), (x+shape_width, bottom)], outline=255, fill=0)
-x += shape_width+padding
-# Draw an X.
-draw.line((x, bottom, x+shape_width, top), fill=255)
-draw.line((x, top, x+shape_width, bottom), fill=255)
-x += shape_width+padding
+    # Load default font.
+    font = ImageFont.load_default()
+    
+    # Read the X, Y, Z axis acceleration values and print them.
+    accel, mag = lsm303.read()
+    # Grab the X, Y, Z components from the reading and print them out.
+    accel_x, accel_y, accel_z = accel
+    mag_x, mag_y, mag_z = mag
 
-# Load default font.
-font = ImageFont.load_default()
+    # Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
+    # Some other nice fonts to try: http://www.dafont.com/bitmap.php
+    #font = ImageFont.truetype('Minecraftia.ttf', 8)
 
-# Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
-# Some other nice fonts to try: http://www.dafont.com/bitmap.php
-#font = ImageFont.truetype('Minecraftia.ttf', 8)
+    # Write 3 lines of text.
+    draw.text((x, top),    'X: ' + str(accel_x),  font=font, fill=255)
+    draw.text((x, top+20), 'Y: ' + str(accel_y), font=font, fill=255)
+    draw.text((x, top+40), 'Z: ' + str(accel_z), font=font, fill=255)
 
-# Write two lines of text.
-draw.text((x, top),    'Hello',  font=font, fill=255)
-draw.text((x, top+20), 'World!', font=font, fill=255)
-
-# Display image.
-disp.image(image)
-disp.display()
+    # Display image.
+    disp.image(image)
+    disp.display()
+    
+    # Wait half a second and repeat.
+    time.sleep(0.5)
